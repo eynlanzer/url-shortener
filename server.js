7b8237f7419ad494beab5f8f12056980ceb6d606
Manoel do Nascimento <manu3105@yahoo.com>
2021-06-28 19:37:50 -0300

    mongoose connect

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 // import express from 'express'
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 3001
+
+mongoose.connect('mongodb://localhost/url-shortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/shortURLs', (req, res) => {
+
 })
 
 app.listen(port, () => {
