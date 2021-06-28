e211fe3c99d3a6837f8b5a1c3ebebb2a902f4635
Manoel do Nascimento <manu3105@yahoo.com>
2021-06-28 17:36:36 -0300

    HTML page

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 app.listen(port, () => {
   const date = new Date()
   console.log(`${date} - Server is running on port: ${port}`)
